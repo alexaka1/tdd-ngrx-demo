f4e5f91e1340d7d46eaf8900623b427f10cc48f7
Martossy Alex <github@mail.martossy.hu>
2022-12-06 10:47:20 +0100

    ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
-
+import { counterReducer } from './counter.reducer';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, StoreModule.forRoot({}, {})],
+  declarations: [AppComponent, MyCounterComponent],
+  imports: [BrowserModule, StoreModule.forRoot({ count: counterReducer }, {})],
   providers: [],
   bootstrap: [AppComponent],
 })
